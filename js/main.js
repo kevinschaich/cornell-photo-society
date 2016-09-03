47f023600b92cf3c044e72b7a5cb4126eecbfc5d
kevinschaich <schaich.kevin@gmail.com>
2016-09-03 18:16:30 -0400

    added favicon

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+$( "#mce-EMAIL" ).on('input', function() {
+    if ($("#mce-EMAIL").val() != "") {
+        $("#mce-EMAIL-required").hide();
+    }
+    else {
+        $("#mce-EMAIL-required").show();
+    }
+});
 
+$( "#mce-FNAME" ).on('input', function() {
+    if ($("#mce-FNAME").val() != "") {
+        $("#mce-FNAME-required").hide();
+    }
+    else {
+        $("#mce-FNAME-required").show();
+    }
+});
+
+$( "#mce-LNAME" ).on('input', function() {
+    if ($("#mce-LNAME").val() != "") {
+        $("#mce-LNAME-required").hide();
+    }
+    else {
+        $("#mce-LNAME-required").show();
+    }
+});
